8e0ab37062302361b95bab7fdae71bf5ef9f7981
unknown <pankornsupercool@gmail.com>
2023-01-01 16:21:37 +0700

    Database Connected

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+const app = express()
+
+//connect mongoDB Atlas
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: false
+})
+    .then(() => console.log("Connected Succesfully"))
+    .catch((err) => console.log(err))
+
+//middleware
+app.use(express.json())
+app.use(cors())
+app.use(morgan("dev"))
+
+//route
+app.get("*", (req, res) => {
+    res.json({
+        data: "message from server"
+    })
+})
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`start server in port ${port}`))
